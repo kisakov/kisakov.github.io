d506e9405411ca8f1f9fcaf86a934bca132a69a0
kisakov <isakov90@gmail.com>
2017-12-24 23:19:42 -0800

    gemfile lock

--- bin/deploy.sh
+++ bin/deploy.sh
@@ -22,7 +22,7 @@ git checkout -b gh-pages
 bundle exec jekyll build
 
 # Delete and move files.
-find . -maxdepth 1 ! -name '_site' ! -name '.git' ! -name '.gitignore' ! -name '.' -exec rm -rf {} \;
+find . -maxdepth 1 ! -name '_site' ! -name '.git' ! -name '.gitignore' ! -name 'Gemfile.lock' ! -name '.' -exec rm -rf {} \;
 mv _site/* .
 rm -R _site/
 
